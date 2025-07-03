51013bfe0361ba684a2b5f27fdcac29c9cb627bf
Nickhil <teamstormxd@gmail.com>
2025-07-03 13:31:15 +0530

    Delete handlers/settings.py

--- handlers/settings.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters
-from pyrogram.types import Message
-from pyrogram.handlers import MessageHandler
-
-def settings_cmd(client, message: Message):
-    message.reply_text("⚙️ Settings feature coming soon...")  # Placeholder response
-
-# Export handler object
-settings_handler = MessageHandler(settings_cmd, filters.command("settings") & filters.group)
