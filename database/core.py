50ac2a36d82d616fe1a5272f398cdf7e1d41987f
Nickhil <teamstormxd@gmail.com>
2025-07-03 16:39:56 +0530

    Update core.py

--- database/core.py
+++ database/core.py
@@ -1,18 +1,32 @@
-whitelist = set()
-violations = {}
+from pymongo import MongoClient
+from config import MONGO_URL
 
-def add_to_whitelist(user_id):
-    whitelist.add(user_id)
+client = MongoClient(MONGO_URL)
+db = client.linkscanbot
+whitelist = db.whitelist
+violations = db.violations
 
-def remove_from_whitelist(user_id):
-    whitelist.discard(user_id)
 
 def is_whitelisted(user_id):
-    return user_id in whitelist
+    return whitelist.find_one({"user_id": user_id}) is not None
 
-def increment_violations(user_id):
-    violations[user_id] = violations.get(user_id, 0) + 1
-    return violations[user_id]
+def add_whitelist(user_id):
+    whitelist.update_one({"user_id": user_id}, {"$set": {"user_id": user_id}}, upsert=True)
+
+def remove_whitelist(user_id):
+    whitelist.delete_one({"user_id": user_id})
 
 def get_all_whitelist():
-    return list(whitelist)
+    return [x['user_id'] for x in whitelist.find()]
+
+def increment_violations(user_id):
+    result = violations.find_one_and_update(
+        {"user_id": user_id},
+        {"$inc": {"count": 1}},
+        upsert=True,
+        return_document=True
+    )
+    return result['count'] if result else 1
+
+def reset_violations(user_id):
+    violations.delete_one({"user_id": user_id})
