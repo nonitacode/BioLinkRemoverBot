7358e3a522ef2d654449ffada9d14d8efe44940c
Nickhil <teamstormxd@gmail.com>
2025-07-03 12:12:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,7 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
-# Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL", None)
 MAX_VIOLATIONS = int(getenv("MAX_VIOLATIONS", 3))
 LOG_CHANNEL = int(getenv("LOG_CHANNEL", 0))
