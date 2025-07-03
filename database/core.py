330115051d0199909fdf8569d47250d7d328e76a
Nickhil <teamstormxd@gmail.com>
2025-07-03 17:30:48 +0530

    Update core.py

--- database/core.py
+++ database/core.py
@@ -2,31 +2,27 @@ from pymongo import MongoClient
 from config import MONGO_URL
 
 client = MongoClient(MONGO_URL)
-db = client.linkscanbot
-whitelist = db.whitelist
+db = client.linkscan
 violations = db.violations
+whitelist = db.whitelist
 
+def is_whitelisted(user_id: int) -> bool:
+    return whitelist.find_one({"_id": user_id}) is not None
 
-def is_whitelisted(user_id):
-    return whitelist.find_one({"user_id": user_id}) is not None
+def add_to_whitelist(user_id: int):
+    whitelist.update_one({"_id": user_id}, {"$set": {"_id": user_id}}, upsert=True)
 
-def add_whitelist(user_id):
-    whitelist.update_one({"user_id": user_id}, {"$set": {"user_id": user_id}}, upsert=True)
-
-def remove_whitelist(user_id):
-    whitelist.delete_one({"user_id": user_id})
+def remove_from_whitelist(user_id: int):
+    whitelist.delete_one({"_id": user_id})
 
 def get_all_whitelist():
-    return [x['user_id'] for x in whitelist.find()]
+    return [u["_id"] for u in whitelist.find()]
 
-def increment_violations(user_id):
-    result = violations.find_one_and_update(
-        {"user_id": user_id},
-        {"$inc": {"count": 1}},
-        upsert=True,
-        return_document=True
-    )
-    return result['count'] if result else 1
+def increment_violations(user_id: int) -> int:
+    record = violations.find_one({"_id": user_id}) or {"_id": user_id, "count": 0}
+    record["count"] += 1
+    violations.update_one({"_id": user_id}, {"$set": record}, upsert=True)
+    return record["count"]
 
-def reset_violations(user_id):
-    violations.delete_one({"user_id": user_id})
+def remove_user_record(user_id: int):
+    violations.delete_one({"_id": user_id})
