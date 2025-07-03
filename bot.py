b7b6b94bf579c4f1b79952c5c9821a78986899e6
Nickhil <teamstormxd@gmail.com>
2025-07-03 21:18:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 from handlers import start, help, admin, moderation
 
-app = Client("linkscanbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("biolinkremoverbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 start.init(app)
 help.init(app)
