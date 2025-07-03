6cc858f2ddcc3a50504ec325d1f7f0f296b0bc38
Nickhil <teamstormxd@gmail.com>
2025-07-03 16:42:28 +0530

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -1,9 +1,6 @@
 import re
 
-# Matches URLs, Telegram usernames, domains, etc.
-link_pattern = re.compile(
-    r"(http[s]?://|t\.me/|@|\.com|\.net|\.org|\.in|\.xyz|\.link|\.me|\.gg|discord\.gg)"
-)
-
-def contains_link(text: str) -> bool:
-    return bool(link_pattern.search(text or ""))
+def contains_link(text):
+    if not text:
+        return False
+    return bool(re.search(r"(http[s]?://|t\.me/|@\w+)", text))
