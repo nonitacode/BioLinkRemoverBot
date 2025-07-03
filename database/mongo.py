8ea943747d73735fa9527ffe364dee5fc8f36ef0
Nickhil <teamstormxd@gmail.com>
2025-07-03 12:35:41 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,18 +1,25 @@
 from pymongo import MongoClient
-from config import MONGO_URL
+import os
 
-mongo = MongoClient(MONGO_URL)
-db = mongo.LinkScanBot
+# MongoDB connection
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
+client = MongoClient(MONGO_URL)
+db = client.link_scan_db
 
+# Users and whitelist collections
 users = db.users
 chats = db.chats
 whitelist = db.whitelist
 
-def whitelist_user(chat_id, user_id):
-    whitelist.update_one({"chat_id": chat_id, "user_id": user_id}, {"$set": {"chat_id": chat_id, "user_id": user_id}}, upsert=True)
+def is_whitelisted(chat_id, user_id):
+    return bool(whitelist.find_one({"chat_id": chat_id, "user_id": user_id}))
+
+def add_whitelist(chat_id, user_id):
+    whitelist.update_one(
+        {"chat_id": chat_id, "user_id": user_id},
+        {"$set": {"chat_id": chat_id, "user_id": user_id}},
+        upsert=True
+    )
 
 def remove_whitelist(chat_id, user_id):
     whitelist.delete_one({"chat_id": chat_id, "user_id": user_id})
-
-def is_whitelisted(chat_id, user_id):
-    return whitelist.find_one({"chat_id": chat_id, "user_id": user_id}) is not None
