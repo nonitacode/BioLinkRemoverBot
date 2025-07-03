95bd6143adb456ed2ecb763017d1fc4e66e4ef79
Nickhil <teamstormxd@gmail.com>
2025-07-03 13:33:28 +0530

    Update and rename permissions.py to db.py

--- utils/permissions.py
+++
@@ -1,5 +0,0 @@
-from pyrogram.types import Chat
-
-async def check_permissions(bot, chat: Chat):
-    member = await bot.get_chat_member(chat.id, bot.me.id)
-    return member.can_delete_messages and member.can_restrict_members
