271c8d48ab65cbc7183c7a2cf865a9e428ab50ea
Nickhil <teamstormxd@gmail.com>
2025-07-03 13:33:54 +0530

    Create tools.py

---
+++ utils/tools.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+
+async def is_admin(client: Client, chat_id: int, user_id: int) -> bool:
+    try:
+        member = await client.get_chat_member(chat_id, user_id)
+        return member.status in ("administrator", "creator")
+    except:
+        return False
