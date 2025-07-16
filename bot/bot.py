1fda09e079f61d92b5d1319f719d940643b709e5
Nickhil <teamstormxd@gmail.com>
2025-07-17 01:48:11 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -5,5 +5,5 @@ app = Client(
     "BioLinkRemoverBot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN
-)
+    bot_token=config.BOT_TOKEN,
+    plugins={"root": "handlers"}
