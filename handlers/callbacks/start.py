91457b16097b749c32fb937a5b2b9eb9a9a0b3cf
Nickhil <teamstormxd@gmail.com>
2025-07-17 03:25:04 +0530

    Update start.py

--- handlers/callbacks/start.py
+++ handlers/callbacks/start.py
@@ -10,18 +10,7 @@ from config import BOT_USERNAME, SUPPORT_GROUP, UPDATES_CHANNEL
 @Client.on_callback_query(filters.regex("start_panel"))
 async def start_panel_cb(client, query: CallbackQuery):
     user = query.from_user
-    lang = get_user_language(user.id)
-    welcome_text = get_message(lang, "welcome_message").format(user=user.mention)
-
-    buttons = InlineKeyboardMarkup([
-        [InlineKeyboardButton("➕ Add Me to Group", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [
-            InlineKeyboardButton("📚 Help", callback_data="help_panel"),
-            InlineKeyboardButton("👤 Developer", url="https://t.me/StormBreakerz")
-        ],
-        [
-            InlineKeyboardButton("💬 Support Group", url=SUPPORT_GROUP),
-            InlineKeyboardButton("📢 Updates", url=UPDATES_CHANNEL)
-        ]
-    ])
+    lang = get_user_language(query.from_user.id)
+    welcome_text = get_message(lang, "welcome_text")
+    
     await query.message.edit_caption(caption=welcome_text, reply_markup=buttons)
