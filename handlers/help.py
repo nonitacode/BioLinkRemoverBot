ad38ba7709a6766f8210cb01cd05d974a07aa1c3
Nickhil <teamstormxd@gmail.com>
2025-07-16 23:58:22 +0530

    Create help.py

---
+++ handlers/help.py
@@ -0,0 +1,30 @@
+# BioLinkRemoverBot - All rights reserved
+# © Graybots™. All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+from bot import bot
+from config import LOG_CHANNEL
+from database.user_language import get_user_language
+from utils.language import get_message
+from buttons.command_buttons import commands_buttons
+
+
+@bot.on_message(filters.command("help"))
+async def help_command(client, message: Message):
+    lang = get_user_language(message.from_user.id)
+    help_text = get_message(lang, "help_message")
+
+    try:
+        await message.reply_photo(
+            photo="assets/biolinkremoverbot.png",
+            caption=help_text,
+            reply_markup=commands_buttons()
+        )
+    except:
+        await message.reply(help_text, reply_markup=commands_buttons())
+
+    await bot.send_message(
+        LOG_CHANNEL,
+        f"#HELP used by [{message.from_user.first_name}](tg://user?id={message.from_user.id})"
+    )
