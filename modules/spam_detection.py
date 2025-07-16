b11e84e6294ea265bb8c4d0383ee7c4dedf49db5
Nickhil <teamstormxd@gmail.com>
2025-07-16 10:31:53 +0530

    Create spam_detection.py

---
+++ modules/spam_detection.py
@@ -0,0 +1,20 @@
+# BioLinkRemoverBot - All rights reserved
+# --------------------------------------
+# This code is fully owned by BioLinkRemoverBot and is reserved.
+# Unauthorized copying, distribution, or use is prohibited.
+# © Graybots™. All rights reserved.
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from database.violation_db import log_violation
+import re
+
+SPAM_KEYWORDS = ["http", "@", "promo", "buy", "sale"]
+
+@bot.on_message(filters.text)
+async def detect_spam(client, message: Message):
+    text = message.text.lower()
+    if any(keyword in text for keyword in SPAM_KEYWORDS):
+        await message.delete()  # Delete the message
+        log_violation(message.from_user.id, "Spam message detected")
+        await message.reply("Your message has been deleted due to potential spam.")
