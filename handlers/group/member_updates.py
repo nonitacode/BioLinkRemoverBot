193d5d3dd5119ddecf970467e9031340b13a4699
Nickhil <teamstormxd@gmail.com>
2025-07-17 03:08:08 +0530

    Update member_updates.py

--- handlers/group/member_updates.py
+++ handlers/group/member_updates.py
@@ -13,13 +13,13 @@ async def handle_chat_member(client, event: ChatMemberUpdated):
 
     if event.new_chat_member:
         user = event.new_chat_member.user
-        await bot.send_message(
+        await app.send_message(
             LOG_CHANNEL,
             f"#JOINED\nUser: [{user.first_name}](tg://user?id={user.id})\nChat: {event.chat.title}"
         )
     elif event.old_chat_member and event.old_chat_member.status != "left" and event.new_chat_member.status == "left":
         user = event.old_chat_member.user
-        await bot.send_message(
+        await app.send_message(
             LOG_CHANNEL,
             f"#LEFT\nUser: [{user.first_name}](tg://user?id={user.id})\nChat: {event.chat.title}"
         )
