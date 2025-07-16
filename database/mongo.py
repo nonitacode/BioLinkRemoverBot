d7d5356b57c1c251fb4dc21a07474e127700a15b
Nickhil <teamstormxd@gmail.com>
2025-07-17 00:23:54 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,20 +1,14 @@
 # BioLinkRemoverBot - All rights reserved
-# --------------------------------------
-# This code is fully owned by BioLinkRemoverBot and is reserved.
-# Unauthorized copying, distribution, or use is prohibited.
 # © Graybots™. All rights reserved.
 
 from pymongo import MongoClient
 from config import MONGO_URL
 
-# Connect to MongoDB
 client = MongoClient(MONGO_URL)
-db = client.biolinkremoverbot
+db = client["BioLinkRemover"]
 
-# MongoDB collections
-users_col = db.users
-groups_col = db.groups
-violation_col = db.violations
-whitelist_col = db.whitelist
-stats_col = db.stats
-language_col = db.language
+users_col = db["users"]
+groups_col = db["groups"]
+violation_col = db["violations"]
+user_language_col = db["user_languages"]
+whitelist_col = db["whitelists"]
