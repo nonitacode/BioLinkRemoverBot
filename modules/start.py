754a40107b17f1dbd9dcb62489e919af793ebc16
Nickhil <teamstormxd@gmail.com>
2025-07-16 10:30:02 +0530

    Create start.py

---
+++ modules/start.py
@@ -0,0 +1,17 @@
+# BioLinkRemoverBot - All rights reserved
+# --------------------------------------
+# This code is fully owned by BioLinkRemoverBot and is reserved.
+# Unauthorized copying, distribution, or use is prohibited.
+# © Graybots™. All rights reserved.
+
+from pyrogram import Client, filters
+from inline import start_buttons
+from config import BOT_NAME
+
+@bot.on_message(filters.command("start"))
+async def start(client, message):
+    welcome_message = f"**Welcome to {BOT_NAME}!**\n\n" \
+                      "Here you can interact with the bot, get stats, manage users, and more.\n" \
+                      "Click the buttons below to get started!"
+    keyboard = start_buttons()
+    await message.reply(welcome_message, reply_markup=keyboard)
