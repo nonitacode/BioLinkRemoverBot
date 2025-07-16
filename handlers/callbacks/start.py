ed59d6db445488035e22fe6dc3642fd37546ecb9
Nickhil <teamstormxd@gmail.com>
2025-07-17 03:52:01 +0530

    Update start.py

--- handlers/callbacks/start.py
+++ handlers/callbacks/start.py
@@ -1,16 +1,24 @@
 # BioLinkRemoverBot - All rights reserved
 # © Graybots™. All rights reserved.
 
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
-from database.user_language import get_user_language
+from bot.bot import app
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, InputMediaPhoto
 from utils.language import get_message
-from config import BOT_USERNAME, SUPPORT_GROUP, UPDATES_CHANNEL
+from database.user_language import get_user_language
+from config import START_IMG
+from utils.inline_buttons import start_buttons
 
-@Client.on_callback_query(filters.regex("start_panel"))
-async def start_panel_cb(client, query: CallbackQuery):
+@app.on_callback_query(filters.regex("main_menu"))
+async def main_menu_cb(client, query: CallbackQuery):
     user = query.from_user
-    lang = get_user_language(query.from_user.id)
-    welcome_text = get_message(lang, "welcome_text")
-    
-    await query.message.edit_caption(caption=welcome_text, reply_markup=buttons)
+    lang = get_user_language(user.id)
+    welcome_message = get_message(lang, "welcome_message").format(user=user.mention)
+
+    try:
+        await query.message.edit_media(
+            media=InputMediaPhoto(media=START_IMG, caption=welcome_message),
+            reply_markup=start_buttons()
+        )
+    except:
+        await query.message.edit_text(welcome_message, reply_markup=start_buttons())
