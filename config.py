883768a691aec90af9dde09402fcceaaa5686500
Nickhil <teamstormxd@gmail.com>
2025-07-17 01:13:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
-
+MAX_VIOLATIONS = int(os.getenv("MAX_VIOLATIONS"))
 BOT_NAME = "Bio Link Remover"
