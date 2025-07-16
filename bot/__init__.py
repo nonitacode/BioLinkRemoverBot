37d8c9f936622ea3e3aa702e9c0cf4ed6905c47c
Nickhil <teamstormxd@gmail.com>
2025-07-16 10:47:42 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,12 @@
+# BioLinkRemoverBot - All rights reserved
+# --------------------------------------
+# This code is fully owned by BioLinkRemoverBot and is reserved.
+# Unauthorized copying, distribution, or use is prohibited.
+# © Graybots™. All rights reserved.
+
+# This file is used to initialize the bot module
+# All other bot-related files will be imported here
+from .bot import *
+from .logger import *
+from .config import *
+from .violation import *
