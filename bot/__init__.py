bf19de9114728034d791d385674894a99890f722
Nickhil <teamstormxd@gmail.com>
2025-07-16 23:47:09 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,12 +1,16 @@
 # BioLinkRemoverBot - All rights reserved
 # --------------------------------------
-# This code is fully owned by BioLinkRemoverBot and is reserved.
 # Unauthorized copying, distribution, or use is prohibited.
 # © Graybots™. All rights reserved.
 
-# This file is used to initialize the bot module
-# All other bot-related files will be imported here
-from .bot import *
-from .logger import *
-from .config import *
-from .violation import *
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "BioLinkRemoverBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=50,
+    plugins={"root": "handlers"}
+)
