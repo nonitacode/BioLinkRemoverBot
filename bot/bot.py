14ec7a31e518e990a352ae3cfd4f09dbbd53b56d
Nickhil <teamstormxd@gmail.com>
2025-07-16 23:55:43 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,31 +1,16 @@
 # BioLinkRemoverBot - All rights reserved
 # --------------------------------------
-# This code is fully owned by BioLinkRemoverBot and is reserved.
 # Unauthorized copying, distribution, or use is prohibited.
 # © Graybots™. All rights reserved.
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-import logging
 
-# Set up logging to console
-logging.basicConfig(
-    format="%(asctime)s - [%(levelname)s] - %(message)s",
-    level=logging.INFO
-)
-
-# Initialize Pyrogram Client (Bot)
 bot = Client(
     "BioLinkRemoverBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    in_memory=True,
-    plugins=dict(root="modules")
+    workers=50,
+    plugins={"root": "handlers"}
 )
-
-# Log when bot starts
-@bot.on_raw_update()
-async def on_start(_, __):
-    logging.info("✅ BioLinkRemoverBot is running...")
-
