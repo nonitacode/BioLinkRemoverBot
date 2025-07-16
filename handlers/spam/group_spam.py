8175c51bcc304949a901f1b1acfce0bc2bf0622e
Nickhil <teamstormxd@gmail.com>
2025-07-17 01:05:38 +0530

    Update group_spam.py

--- handlers/spam/group_spam.py
+++ handlers/spam/group_spam.py
@@ -3,10 +3,10 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-from bot.bot import bot
+from bot.bot import app
 from utils.spam_checker import is_spam, check_and_handle_violation
 
-@bot.on_message(filters.text & filters.group)
+@app.on_message(filters.text & filters.group)
 async def group_text_check(client, message: Message):
     if is_spam(message.text):
         await check_and_handle_violation(message)
