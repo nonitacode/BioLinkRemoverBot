3c526a89f13a64b601649586ffd892069fb5f692
Nickhil <teamstormxd@gmail.com>
2025-07-16 19:23:21 +0530

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -5,25 +5,30 @@
 # © Graybots™. All rights reserved.
 
 from pyrogram import filters
-from pyrogram.types import Message
-from modules.inline import start_buttons
-from config import BOT_NAME
 from bot.bot import bot
-from database.users_db import store_user_data
+from modules.inline import start_buttons
+from config import BOT_NAME, LOG_CHANNEL
 from database.user_language_db import get_user_language
 from utils.language import get_message
+from database.users_db import store_user_data
+from pyrogram.types import InputMediaPhoto
 
 @bot.on_message(filters.command("start"))
-async def start_command(client, message: Message):
+async def start(client, message):
     user = message.from_user
     store_user_data(user.id, user.username, user.full_name)
+
     lang = get_user_language(user.id)
-    welcome_text = get_message(lang, "welcome_message")
-    try:
-        await message.reply_photo(
-            photo="assets/biolinkremoverbot.png",
-            caption=welcome_text,
-            reply_markup=start_buttons()
-        )
-    except:
-        await message.reply(welcome_text, reply_markup=start_buttons())
+    welcome = get_message(lang, "welcome_message")
+
+    await message.reply_photo(
+        photo="assets/biolinkremoverbot.png",
+        caption=welcome,
+        reply_markup=start_buttons()
+    )
+
+    # Log to LOG_CHANNEL
+    await bot.send_message(
+        LOG_CHANNEL,
+        f"#STARTED\n\n👤 User: {user.mention} (`{user.id}`)\n📩 Started bot in private chat."
+    )
