a42135d17f261531ecbc7754af82a8bd94389d99
Nickhil <teamstormxd@gmail.com>
2025-07-16 10:31:13 +0530

    Create user_commands.py

---
+++ modules/user_commands.py
@@ -0,0 +1,36 @@
+# BioLinkRemoverBot - All rights reserved
+# --------------------------------------
+# This code is fully owned by BioLinkRemoverBot and is reserved.
+# Unauthorized copying, distribution, or use is prohibited.
+# © Graybots™. All rights reserved.
+
+from pyrogram import Client, filters
+from inline import user_commands_buttons
+from config import COMMAND_PREFIXES
+from database.users_db import store_user_data, store_group_data
+
+@bot.on_message(filters.command("addauth"))
+async def add_auth(client, message):
+    # Logic to add a user to the whitelist
+    store_user_data(message.from_user.id, message.from_user.username, message.from_user.full_name)
+    await message.reply("User added to the whitelist.")
+
+@bot.on_message(filters.command("removeauth"))
+async def remove_auth(client, message):
+    # Logic to remove a user from the whitelist
+    await message.reply("User removed from the whitelist.")
+
+@bot.on_message(filters.command("warn"))
+async def warn_user(client, message):
+    # Logic to warn a user
+    await message.reply("User has been warned.")
+
+@bot.on_message(filters.command("profile"))
+async def user_profile(client, message):
+    # Show user profile information
+    await message.reply("Here is your profile information.")
+
+@bot.on_message(filters.callback_data("user_commands"))
+async def user_commands(client, message):
+    keyboard = user_commands_buttons()
+    await message.reply("These are the user-related commands:", reply_markup=keyboard)
