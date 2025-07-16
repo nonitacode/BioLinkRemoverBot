f135d5a924836f153b64d44d86eb6736f72271eb
Nickhil <teamstormxd@gmail.com>
2025-07-16 19:24:41 +0530

    Update help.py

--- modules/help.py
+++ modules/help.py
@@ -5,13 +5,17 @@
 # © Graybots™. All rights reserved.
 
 from pyrogram import filters
-from modules.inline import commands_buttons
 from bot.bot import bot
+from modules.inline import commands_buttons
 from database.user_language_db import get_user_language
 from utils.language import get_message
 
 @bot.on_message(filters.command("help"))
-async def help_command(client, message):
+async def help(client, message):
     lang = get_user_language(message.from_user.id)
     help_text = get_message(lang, "help_message")
-    await message.reply(help_text, reply_markup=commands_buttons())
+
+    await message.reply(
+        text=help_text,
+        reply_markup=commands_buttons()
+    )
