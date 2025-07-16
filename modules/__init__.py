1d140a5aa19477c091b922ff5699204c981c4142
Nickhil <teamstormxd@gmail.com>
2025-07-16 10:29:30 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,14 @@
+# BioLinkRemoverBot - All rights reserved
+# --------------------------------------
+# This code is fully owned by BioLinkRemoverBot and is reserved.
+# Unauthorized copying, distribution, or use is prohibited.
+# © Graybots™. All rights reserved.
+
+# This file is used to initialize the modules directory.
+# All the module files will be imported here for modular organization.
+
+from .start import *
+from .help import *
+from .user_commands import *
+from .spam_detection import *
+from .inline import *
