da2fe5c0ffe6d111c3c3c7d202673e7a6ac067ed
Nickhil <teamstormxd@gmail.com>
2025-07-16 10:30:28 +0530

    Create help.py

---
+++ modules/help.py
@@ -0,0 +1,14 @@
+# BioLinkRemoverBot - All rights reserved
+# --------------------------------------
+# This code is fully owned by BioLinkRemoverBot and is reserved.
+# Unauthorized copying, distribution, or use is prohibited.
+# © Graybots™. All rights reserved.
+
+from pyrogram import Client, filters
+from inline import commands_buttons
+
+@bot.on_message(filters.command("help"))
+async def help(client, message):
+    help_message = "Welcome to the Help Panel. Choose a category to see the commands."
+    keyboard = commands_buttons()
+    await message.reply(help_message, reply_markup=keyboard)
