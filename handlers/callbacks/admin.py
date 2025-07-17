8f0881fa0fd7f9ecf1ba944c1a3e3034461e1eab
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:37:39 +0530

    Update admin.py

--- handlers/callbacks/admin.py
+++ handlers/callbacks/admin.py
@@ -4,14 +4,14 @@ from bot.bot import app
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
 from utils.language import get_message
-from database.user_language import get_user_language
+from database.user_language import get_user_language  # async function
 
 @app.on_callback_query(filters.regex("help_allow"))
 async def help_allow_cb(client, query: CallbackQuery):
-    lang = get_user_language(query.from_user.id)
+    lang = await get_user_language(query.from_user.id)
     await query.message.edit_text(get_message(lang, "help_allow"))
 
 @app.on_callback_query(filters.regex("help_mute"))
 async def help_mute_cb(client, query: CallbackQuery):
-    lang = get_user_language(query.from_user.id)
+    lang = await get_user_language(query.from_user.id)
     await query.message.edit_text(get_message(lang, "help_mute"))
