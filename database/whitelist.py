f6a762ebb6ae0bda1f24b456bada8afd76780d91
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:35:25 +0530

    Update whitelist.py

--- database/whitelist.py
+++ database/whitelist.py
@@ -1,25 +1,30 @@
 # BioLinkRemoverBot - All rights reserved
 # © Graybots™. All rights reserved.
 
-from database.mongo import whitelists_col
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
 
-def add_to_whitelist(chat_id: int, user_id: int):
-    whitelist_col.update_one(
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["BioLinkRemover"]
+whitelist_col = db["whitelists"]
+
+async def add_to_whitelist(chat_id: int, user_id: int):
+    await whitelist_col.update_one(
         {"chat_id": chat_id},
         {"$addToSet": {"users": user_id}},
         upsert=True
     )
 
-def remove_from_whitelist(chat_id: int, user_id: int):
-    whitelist_col.update_one(
+async def remove_from_whitelist(chat_id: int, user_id: int):
+    await whitelist_col.update_one(
         {"chat_id": chat_id},
         {"$pull": {"users": user_id}}
     )
 
-def get_whitelisted_users(chat_id: int) -> list:
-    group = whitelist_col.find_one({"chat_id": chat_id}) or {}
+async def get_whitelisted_users(chat_id: int) -> list:
+    group = await whitelist_col.find_one({"chat_id": chat_id}) or {}
     return group.get("users", [])
 
-def is_user_whitelisted(chat_id: int, user_id: int) -> bool:
-    group = whitelist_col.find_one({"chat_id": chat_id})
+async def is_user_whitelisted(chat_id: int, user_id: int) -> bool:
+    group = await whitelist_col.find_one({"chat_id": chat_id})
     return user_id in group.get("users", []) if group else False
