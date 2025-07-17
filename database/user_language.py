508addc40bc9b8c892bdf4604cd8aa1b2e97edaf
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:14:28 +0530

    Update user_language.py

--- database/user_language.py
+++ database/user_language.py
@@ -1,12 +1,14 @@
-from database.mongo import users_collection
+from database.mongo import user_language_col
 
-async def get_user_language(user_id: int) -> str:
-    user = await users_collection.find_one({"_id": user_id})
-    return user.get("language", "en") if user else "en"
+def get_user_language(user_id: int) -> str:
+    user = user_language_col.find_one({"_id": user_id})
+    if user and "language" in user:
+        return user["language"]
+    return "en"  # default to English
 
-async def set_user_language(user_id: int, language_code: str):
-    await users_collection.update_one(
+def set_user_language(user_id: int, lang_code: str):
+    user_language_col.update_one(
         {"_id": user_id},
-        {"$set": {"language": language_code}},
+        {"$set": {"language": lang_code}},
         upsert=True
     )
