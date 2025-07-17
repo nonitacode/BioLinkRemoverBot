26b8a9f105ace4878cbd6a3fb182267572acef94
Nickhil <teamstormxd@gmail.com>
2025-07-17 12:04:36 +0530

    Create group_text_check.py

---
+++ handlers/spam/group_text_check.py
@@ -0,0 +1,12 @@
+# BioLinkRemoverBot - All rights reserved
+# © Graybots™. All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+from bot.bot import app
+from utils.spam_checker import is_spam, check_and_handle_violation
+
+@app.on_message(filters.text & filters.group)
+async def group_text_check(client, message: Message):
+    if is_spam(message.text):
+        await check_and_handle_violation(client, message)
