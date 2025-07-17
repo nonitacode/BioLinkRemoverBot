b90f155f37e6a1296443a770a4fab12e1d5efba8
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:25:46 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,14 +1,26 @@
+# BioLinkRemoverBot - All rights reserved
+# © Graybots™. All rights reserved.
+
 from database.mongo import users_col
 
-async def get_user(user_id: int) -> dict:
+async def store_user_data(user_id: int, username: str = None, first_name: str = ""):
+    await users_col.update_one(
+        {"user_id": user_id},
+        {
+            "$set": {
+                "username": username,
+                "first_name": first_name
+            }
+        },
+        upsert=True
+    )
+
+async def get_user(user_id: int):
     return await users_col.find_one({"user_id": user_id})
 
-async def add_user(user_id: int, first_name: str):
-    if not await get_user(user_id):
-        await users_col.insert_one({
-            "user_id": user_id,
-            "first_name": first_name
-        })
-
 async def get_all_users():
-    return users_col.find()
+    cursor = users_col.find({})
+    return [user async for user in cursor]
+
+async def delete_user(user_id: int):
+    await users_col.delete_one({"user_id": user_id})
