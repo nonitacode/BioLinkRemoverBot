762dcf2048aa35183079e95806c8deef128048bb
Nickhil <teamstormxd@gmail.com>
2025-07-17 08:25:55 +0530

    Update whitelist.py

--- database/whitelist.py
+++ database/whitelist.py
@@ -1,6 +1,3 @@
-# BioLinkRemoverBot - All rights reserved
-# © Graybots™. All rights reserved.
-
 from database.mongo import whitelist_col
 
 def add_to_whitelist(chat_id, user_id):
@@ -16,8 +13,10 @@ def remove_from_whitelist(chat_id, user_id):
         {"$pull": {"users": user_id}}
     )
 
-def get_whitelisted_users():
-    users = []
-    for group in whitelist_col.find({}):
-        users.extend(group.get("users", []))
-    return [{"user_id": uid} for uid in users]
+def get_whitelisted_users(chat_id):
+    group = whitelist_col.find_one({"chat_id": chat_id}) or {}
+    return group.get("users", [])
+
+async def is_user_whitelisted(chat_id, user_id):
+    group = whitelist_col.find_one({"chat_id": chat_id})
+    return group and user_id in group.get("users", [])
