5f4b72a37233c292ed61203e9988206448ec5f5a
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:24:22 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,7 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
     "BioLinkRemoverBot",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
