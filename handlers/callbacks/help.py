049b88ab219c167f80d7a5683e258aa4ae4dc429
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:41:23 +0530

    Update help.py

--- handlers/callbacks/help.py
+++ handlers/callbacks/help.py
@@ -1,21 +1,17 @@
-# BioLinkRemoverBot - All rights reserved
-# © Graybots™. All rights reserved.
-
-from bot.bot import app
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
+from bot.bot import app
+from database.users import get_user_language
 from utils.language import get_message
-from database.user_language import get_user_language
 from utils.inline_buttons import commands_buttons
 
 @app.on_callback_query(filters.regex("help_panel"))
-async def help_panel_cb(client, query: CallbackQuery):
-    user_id = query.from_user.id
-    lang = get_user_language(user_id)
+async def help_panel_cb(client, callback_query: CallbackQuery):
+    user_id = callback_query.from_user.id
+    lang = await get_user_language(user_id)
     help_text = get_message(lang, "help_message")
 
-    await query.message.edit_text(
+    await callback_query.message.edit_text(
         text=help_text,
-        reply_markup=await commands_buttons(user_id),
-        disable_web_page_preview=True
+        reply_markup=await commands_buttons(user_id)
     )
