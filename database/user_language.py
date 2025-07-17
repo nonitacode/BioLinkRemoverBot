0fe18f17c7521e9a25e30b499cf585f0b134e4c0
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:29:58 +0530

    Update user_language.py

--- database/user_language.py
+++ database/user_language.py
@@ -1,13 +1,16 @@
+# BioLinkRemoverBot - All rights reserved
+# © Graybots™. All rights reserved.
+
 from database.mongo import user_language_col
 
-def get_user_language(user_id: int) -> str:
-    user = user_language_col.find_one({"_id": user_id})
+async def get_user_language(user_id: int) -> str:
+    user = await user_language_col.find_one({"_id": user_id})
     if user and "language" in user:
         return user["language"]
     return "en"  # default to English
 
-def set_user_language(user_id: int, lang_code: str):
-    user_language_col.update_one(
+async def set_user_language(user_id: int, lang_code: str):
+    await user_language_col.update_one(
         {"_id": user_id},
         {"$set": {"language": lang_code}},
         upsert=True
