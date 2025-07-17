d892c4d01c271b1d341477c1d287e05cafd296eb
Nickhil <teamstormxd@gmail.com>
2025-07-17 12:05:56 +0530

    Update and rename private_spam.py to private_text_check.py

--- handlers/spam/private_text_check.py
+++ handlers/spam/private_text_check.py
@@ -4,9 +4,18 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from bot.bot import app
+from config import OWNER_ID
 from utils.spam_checker import is_spam, check_and_handle_violation
 
 @app.on_message(filters.text & filters.private)
 async def private_text_check(client, message: Message):
+    user = message.from_user
+    if not user or user.is_bot:
+        return
+
+    # Skip for OWNER
+    if user.id == OWNER_ID:
+        return
+
     if is_spam(message.text):
-        await check_and_handle_violation(message)
+        await check_and_handle_violation(client, message)
