f9ed54af0077c9f9ab8a733a8eb650be67f49fda
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:07:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-# BioLinkRemoverBot - All rights reserved
-# © Graybots™. All rights reserved.
-
 from bot.bot import app
 from config import BOT_NAME
 
 # ✅ Command Handlers
-from handlers.commands import auth, basic, core, core, help, moderation, owner, start, stats
-from handlers.group.callbacks import unmute
-from handlers.group import bio_scan, whitelist, member_updates
+from handlers.commands import start, help, basic, core, auth, owner, stats, moderation
 
-# ✅ Callback Handlers (explicitly importing modules)
+# ✅ Callback Query Handlers
+import handlers.callbacks.start
+import handlers.callbacks.help
+import handlers.callbacks.language
 import handlers.callbacks.admin
 import handlers.callbacks.basic
 import handlers.callbacks.developer
-import handlers.callbacks.help
-import handlers.callbacks.language
-import handlers.callbacks.start
 import handlers.callbacks.user
 
-# ✅ Group & Spam
+# ✅ Group Filters / Scanners
+from handlers.group import bio_scan, whitelist, member_updates
+from handlers.group.callbacks import unmute
+
+# ✅ Spam Checkers
 from handlers.spam import group_spam, private_spam
 
 # ✅ Miscellaneous
