e840b43cb6c855534384bd5014686e95cf4dc1db
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:31:29 +0530

    Update groups.py

--- database/groups.py
+++ database/groups.py
@@ -1,7 +1,12 @@
 # BioLinkRemoverBot - All rights reserved
 # © Graybots™. All rights reserved.
 
-from database.mongo import groups_col
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["BioLinkRemover"]
+groups_col = db["groups"]
 
 async def store_group_data(chat_id: int, title: str):
     await groups_col.update_one(
@@ -10,5 +15,5 @@ async def store_group_data(chat_id: int, title: str):
         upsert=True
     )
 
-def get_groups_count() -> int:
-    return groups_col.count_documents({})
+async def get_groups_count() -> int:
+    return await groups_col.count_documents({})
