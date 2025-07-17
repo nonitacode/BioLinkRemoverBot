d6812747cf0bf67f8de68c9a2e0b6261a4e55bf9
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:29:01 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -1,15 +1,16 @@
-# mongo.py
+# BioLinkRemoverBot - All rights reserved
+# © Graybots™. All rights reserved.
 
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
 
-client = MongoClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client["BioLinkRemover"]
 
 users_collection = db["users"]
 groups_col = db["groups"]
 violation_col = db["violations"]
 user_language_col = db["user_languages"]
-whitelists_col = db["whitelists"]   # ✅ FIXED: renamed for correct import
+whitelists_col = db["whitelists"]
 auth_users_collection = db["auth_users"]
 warns_col = db["warns"]
