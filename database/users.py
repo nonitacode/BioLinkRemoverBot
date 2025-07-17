8c35020a007bbc17880ca01667efd74b82d4b330
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:28:31 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,15 +1,15 @@
 # BioLinkRemoverBot - All rights reserved
 # © Graybots™. All rights reserved.
 
-from database.mongo import users_col
+from database.mongo import users_col, user_language_col
 
-async def store_user_data(user_id: int, username: str = None, first_name: str = ""):
+async def store_user_data(user_id: int, first_name: str = "", username: str = ""):
     await users_col.update_one(
         {"user_id": user_id},
         {
             "$set": {
-                "username": username,
-                "first_name": first_name
+                "first_name": first_name,
+                "username": username
             }
         },
         upsert=True
@@ -18,9 +18,13 @@ async def store_user_data(user_id: int, username: str = None, first_name: str = 
 async def get_user(user_id: int):
     return await users_col.find_one({"user_id": user_id})
 
-async def get_all_users():
-    cursor = users_col.find({})
-    return [user async for user in cursor]
+async def set_user_language(user_id: int, language_code: str):
+    await user_language_col.update_one(
+        {"user_id": user_id},
+        {"$set": {"language_code": language_code}},
+        upsert=True
+    )
 
-async def delete_user(user_id: int):
-    await users_col.delete_one({"user_id": user_id})
+def get_user_language(user_id: int) -> str:
+    lang = user_language_col.find_one({"user_id": user_id})
+    return lang["language_code"] if lang and "language_code" in lang else "en"
