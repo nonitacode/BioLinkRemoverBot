597e5a0a386e5ef0cba881b4c473121a9bb2320f
Nickhil <teamstormxd@gmail.com>
2025-07-17 13:59:24 +0530

    Update auth.py

--- handlers/commands/auth.py
+++ handlers/commands/auth.py
@@ -1,11 +1,13 @@
 from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram.enums import ChatMemberStatus
+
 from bot.bot import app
+from config import OWNER_ID
 from utils.language import get_message
 from database.user_language import get_user_language
-from database.auth_users import add_auth_user, remove_auth_user, get_auth_users
+from database.auth_users import add_auth_user, remove_auth_user, get_auth_users, refresh_auth_cache
 from database.warns import reset_warns
-from pyrogram.enums import ChatMemberStatus
 
 @app.on_message(filters.command("addauth") & filters.group)
 async def add_auth(client, message: Message):
@@ -18,13 +20,15 @@ async def add_auth(client, message: Message):
     chat_id = message.chat.id
 
     member = await client.get_chat_member(chat_id, message.from_user.id)
-    if member.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+    if message.from_user.id != OWNER_ID and member.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
         return await message.reply(get_message(lang, "admin_only"))
 
     await add_auth_user(chat_id, user.id)
     await reset_warns(chat_id, user.id)
+    await refresh_auth_cache(chat_id)
 
     await message.reply(get_message(lang, "auth_added").format(user=user.mention))
+
 
 @app.on_message(filters.command("rmauth") & filters.group)
 async def remove_auth(client, message: Message):
@@ -37,11 +41,14 @@ async def remove_auth(client, message: Message):
     chat_id = message.chat.id
 
     member = await client.get_chat_member(chat_id, message.from_user.id)
-    if member.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+    if message.from_user.id != OWNER_ID and member.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
         return await message.reply(get_message(lang, "admin_only"))
 
     await remove_auth_user(chat_id, user.id)
+    await refresh_auth_cache(chat_id)
+
     await message.reply(get_message(lang, "auth_removed").format(user=user.mention))
+
 
 @app.on_message(filters.command("authusers") & filters.group)
 async def list_auth_users(client, message: Message):
