316d2ffae148cad9fbee297cb7b5e357778fbb0c
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:18:03 +0530

    Update whitelist.py

--- database/whitelist.py
+++ database/whitelist.py
@@ -1,21 +1,22 @@
 from database.mongo import whitelist_col
 
-def add_to_whitelist(chat_id, user_id):
-    whitelist_col.update_one(
+async def add_whitelisted_user(chat_id: int, user_id: int):
+    await whitelist_col.update_one(
         {"chat_id": chat_id},
-        {"$addToSet": {"users": user_id}},
+        {"$addToSet": {"user_ids": user_id}},
         upsert=True
     )
 
-def remove_from_whitelist(chat_id, user_id):
-    whitelist_col.update_one(
+async def remove_whitelisted_user(chat_id: int, user_id: int):
+    await whitelist_col.update_one(
         {"chat_id": chat_id},
-        {"$pull": {"users": user_id}}
+        {"$pull": {"user_ids": user_id}}
     )
 
-def get_whitelisted_users(chat_id):
-    group = whitelist_col.find_one({"chat_id": chat_id}) or {}
-    return group.get("users", [])
+async def get_whitelisted_users(chat_id: int) -> list:
+    doc = await whitelist_col.find_one({"chat_id": chat_id})
+    return doc["user_ids"] if doc else []
 
-async def is_user_whitelisted(chat_id, user_id):
-    return await whitelist_collection.find_one({"chat_id": chat_id, "user_id": user_id}) is not None
+async def is_user_whitelisted(chat_id: int, user_id: int) -> bool:
+    users = await get_whitelisted_users(chat_id)
+    return user_id in users
