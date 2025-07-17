81e4664eee957e932f96565e625e6324bcfdfb43
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:12:20 +0530

    Update warns.py

--- database/warns.py
+++ database/warns.py
@@ -1,7 +1,7 @@
 # BioLinkRemoverBot - All rights reserved
 # © Graybots™. All rights reserved.
 
-from database.mongo import warns_collection
+from database.mongo import warns_col
 
 async def add_warn(user_id: int, chat_id: int, reason: str = "Violation"):
     await warns_col.insert_one({
