f0d7437a6e26401a8579b3c8839908e6a6d30f4e
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:44:18 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,33 +1,20 @@
-# BioLinkRemoverBot - All rights reserved
-# © Graybots™. All rights reserved.
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
 
-from database.mongo import users_col, user_language_col
+client = AsyncIOMotorClient(MONGO_URI)
+db = client.biolinkbot
+users_collection = db.users
 
-async def store_user_data(user_id: int, first_name: str = "", username: str = ""):
-    await users_col.update_one(
-        {"user_id": user_id},
-        {
-            "$set": {
-                "first_name": first_name,
-                "username": username
-            }
-        },
+async def store_user_data(user_id, username, full_name):
+    await users_collection.update_one(
+        {"_id": user_id},
+        {"$set": {
+            "username": username,
+            "full_name": full_name
+        }},
         upsert=True
     )
 
-async def get_user(user_id: int):
-    return await users_col.find_one({"user_id": user_id})
-
-async def set_user_language(user_id: int, language_code: str):
-    await user_language_col.update_one(
-        {"user_id": user_id},
-        {"$set": {"language_code": language_code}},
-        upsert=True
-    )
-
-def get_user_language(user_id: int) -> str:
-    lang = user_language_col.find_one({"user_id": user_id})
-    return lang["language_code"] if lang and "language_code" in lang else "en"
-
-def get_users_count() -> int:
-    return users_col.count_documents({})
+async def get_user_language(user_id):
+    user = await users_collection.find_one({"_id": user_id})
+    return user.get("language", "en") if user else "en"
