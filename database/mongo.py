16463f0c04182aac348393d99b0e1913633667c2
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:10:26 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -6,7 +6,7 @@ from config import MONGO_URL
 client = MongoClient(MONGO_URL)
 db = client["BioLinkRemover"]
 
-users_col = db["users"]
+users_collection = db["users"]
 groups_col = db["groups"]
 violation_col = db["violations"]
 user_language_col = db["user_languages"]
