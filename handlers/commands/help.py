3ca9e8b47a096eaf28b89cfe1c34ed0e578b17fd
Nickhil <teamstormxd@gmail.com>
2025-07-17 10:37:45 +0530

    Update help.py

--- handlers/commands/help.py
+++ handlers/commands/help.py
@@ -19,12 +19,12 @@ async def help_command(client, message: Message):
         await message.reply_photo(
             photo=START_IMG,
             caption=help_text,
-            reply_markup=await commands_buttons(user_id)
+            reply_markup=commands_buttons(user_id)
         )
     except:
         await message.reply(
             text=help_text,
-            reply_markup=await commands_buttons(user_id)
+            reply_markup=commands_buttons(user_id)
         )
 
     await app.send_message(
