81402233be120c61d494b82c05451bbf915923b7
Nickhil <teamstormxd@gmail.com>
2025-07-17 06:45:48 +0530

    Create developer.py

---
+++ handlers/callbacks/developer.py
@@ -0,0 +1,12 @@
+# Developer Command Callback Handlers
+
+from bot.bot import app
+from pyrogram import filters
+from pyrogram.types import CallbackQuery
+from utils.language import get_message
+from database.user_language import get_user_language
+
+@app.on_callback_query(filters.regex("help_ban"))
+async def help_ban_cb(client, query: CallbackQuery):
+    lang = get_user_language(query.from_user.id)
+    await query.message.edit_text(get_message(lang, "help_ban"))
