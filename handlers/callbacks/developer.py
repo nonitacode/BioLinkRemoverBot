f21f0c176558aa95480e07a25f8f38f0ef5cdd39
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:39:02 +0530

    Update developer.py

--- handlers/callbacks/developer.py
+++ handlers/callbacks/developer.py
@@ -8,5 +8,5 @@ from database.user_language import get_user_language
 
 @app.on_callback_query(filters.regex("help_ban"))
 async def help_ban_cb(client, query: CallbackQuery):
-    lang = get_user_language(query.from_user.id)
+    lang = await get_user_language(query.from_user.id)
     await query.message.edit_text(get_message(lang, "help_ban"))
