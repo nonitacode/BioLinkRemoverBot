9d77480c5bfc61273552f434ed17a3fba3af4d6c
Nickhil <teamstormxd@gmail.com>
2025-07-17 11:34:39 +0530

    Update auth_users.py

--- database/auth_users.py
+++ database/auth_users.py
@@ -1,20 +1,27 @@
 # BioLinkRemoverBot - All rights reserved
 # © Graybots™. All rights reserved.
 
-from database.mongo import auth_users_collection
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
 
-def add_auth_user(user_id: int):
-    auth_users_col.update_one(
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["BioLinkRemover"]
+auth_users_col = db["auth_users"]
+
+async def add_auth_user(user_id: int):
+    await auth_users_col.update_one(
         {"user_id": user_id},
         {"$set": {"user_id": user_id}},
         upsert=True
     )
 
-def remove_auth_user(user_id: int):
-    auth_users_col.delete_one({"user_id": user_id})
+async def remove_auth_user(user_id: int):
+    await auth_users_col.delete_one({"user_id": user_id})
 
-def get_auth_users() -> list:
-    return [doc["user_id"] for doc in auth_users_col.find({}, {"_id": 0, "user_id": 1})]
+async def get_auth_users() -> list:
+    cursor = auth_users_col.find({}, {"_id": 0, "user_id": 1})
+    return [doc["user_id"] async for doc in cursor]
 
-def is_auth_user(user_id: int) -> bool:
-    return auth_users_col.count_documents({"user_id": user_id}) > 0
+async def is_auth_user(user_id: int) -> bool:
+    count = await auth_users_col.count_documents({"user_id": user_id})
+    return count > 0
